bb0cc43807ce7df5802c7e94b7aae1a1f329b5bb
Sarah Braga <86424605+sarahmbss@users.noreply.github.com>
2023-01-14 14:55:25 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,11 @@
+correcao = {
+    'eleicoe': 'eleicao',
+    'hj': 'hoje',
+    'vc': 'voce',
+    'jn': 'jornal nacional',
+    'q': 'que',
+    'tbm': 'tambem',
+    'n': 'nao',
+    'tb': 'tambem',
+    'nn': 'nao'
+}
